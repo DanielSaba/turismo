e52dbdf0ad52224960abbac08286baafe318ee51
Daniel <danielantoniowww@gmail.com>
2025-11-28 02:29:00 -0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.css"
+
+import {inter} from '@/config/fonts'
+import { Provider } from "@/components";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Provider>
         {children}
+      </Provider>
+        
+
       </body>
     </html>
   );
