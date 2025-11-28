d1043439c87afad4d59af99ac8e648d1ddef7efa
Daniel <danielantoniowww@gmail.com>
2025-11-28 08:20:54 -0500

    packagerrgg

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,5 @@
-import "dotenv/config";     // ← ESTA LÍNEA ARREGLA TODO
-import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
 
-export default defineConfig({
+export default {
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
-});
+};
